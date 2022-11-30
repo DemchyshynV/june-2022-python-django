2aa8cfdc9a89ed1b637004e836efb3960bbb0c3c
Vitaliy Demchyshyn <demchyshyn.v87@gmail.com>
2022-11-30 20:23:31 +0200

    lesson3-hw

--- apps/auto_parks/views.py
+++ apps/auto_parks/views.py
@@ -1,9 +1,11 @@
+from rest_framework import status
 from rest_framework.generics import ListCreateAPIView, GenericAPIView
 from rest_framework.response import Response
 
 from .models import AutoParkModel
 from .serializers import AutoParkSerializer
 from apps.cars.serializers import CarSerializer
+from apps.cars.models import CarModel
 
 
 class AutoParkListCreateView(ListCreateAPIView):
@@ -11,8 +13,14 @@ class AutoParkListCreateView(ListCreateAPIView):
     serializer_class = AutoParkSerializer
 
 
-class AddCarToAutoParkView(GenericAPIView):
+class CarListCreateView(GenericAPIView):
     queryset = AutoParkModel.objects.all()
+
+    def get(self, *args, **kwargs):
+        pk = kwargs.get('pk')
+        cars = CarModel.objects.filter(auto_park_id=pk)
+        serializer = CarSerializer(cars, many=True)
+        return Response(serializer.data, status.HTTP_200_OK)
 
     def post(self, *args, **kwargs):
         auto_park = self.get_object()
@@ -21,4 +29,4 @@ class AddCarToAutoParkView(GenericAPIView):
         serializer.is_valid(raise_exception=True)
         serializer.save(auto_park=auto_park)
         serializer = AutoParkSerializer(auto_park)
-        return Response(serializer.data)
+        return Response(serializer.data, status.HTTP_201_CREATED)
