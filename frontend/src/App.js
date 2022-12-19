0f5f4c0f4ce22aa27bcbbf7b8a055d12bd0b8a9d
Vitaliy Demchyshyn <demchyshyn.v87@gmail.com>
2022-12-19 20:40:25 +0200

    lesson9_part1_angular

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const App = () => {
+  const [cars, setCars] = useState([])
+
+  useEffect(() => {
+    axios.get('/api/cars').then(value => setCars(value.data))
+  }, [])
+
+  return (
+    <div>
+      <h1>React!!!!!!!!!</h1>
+      {cars.map(car => <div key={car.id}>{car.brand}</div>)}
+    </div>
+  )
+}
+
+export default App
